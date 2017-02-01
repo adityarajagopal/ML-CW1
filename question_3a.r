223478163f86b6f62e9c7a8320be872fa8de0ccf
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2017-02-01 16:10:41 +0000

    q3a updated

--- question_3a.r
+++ question_3a.r
@@ -1,9 +1,11 @@
 #!/usr/bin/env/Rscript
-dataset <- function(n){
-	x1 <- seq(1,n+1); 
-	line <- 3*x1+5; 
+dataset <- function(n,m,c){
+	x1 <- seq(from=0,to=1,by=1/(n-1)); 
+	x1 <- c(1,x1);
+	line <- m*x1+c; 
 	rand <- rnorm(n+1,mean=0,sd=100);
 	x2 <- line + rand;
+	x2 <- abs(x2 / max(x2));
 	i <- (1:length(x2));
 	plus <- i[x2[i] >= line[i]]; 
 	minus <- i[x2[i] < line[i]];
@@ -50,14 +52,19 @@ learn <- function(ip,limit){
 	return (w);
 }
 
-dset <- dataset(100);
-p <- initialise(dset);
-w <- learn(p,10000);
+perceptron <- function(datapoints,m,c){
+	dset <- dataset(datapoints,m,c);
+	p <- initialise(dset);
+	w <- learn(p,10000);
+	
+	x1 <- dset$feat1; 
+	a <- -(w[2]/w[3]);
+	b <- -(w[1]/w[3]);
+	x2 <- a*x1 + b; 
+	plot(x1,dset$feat2,col=dset$col);
+	lines(x1,x2,col='red');
+	lines(x1,dset$orig,col='blue');
 
-x1 <- dset$feat1; 
-x2 <- -(w[2]/w[3])*x1 - (w[1]/w[2]); 
-plot(x1,dset$feat2,col=dset$col);
-lines(x1,x2,col='red');
-lines(x1,dset$orig,col='blue');
-
+	return (list(coeff1=a,coeff2=b)); 
+}
 
